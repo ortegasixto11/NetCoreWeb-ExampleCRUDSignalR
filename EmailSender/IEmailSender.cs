c66248e84b733b61fa7da0908d104a0ecf4454d7
Sixto Ortega <sixto_miguel@hotmail.com>
2020-04-30 15:56:19 -0400

    Adding EmailSender Helper library

---
+++ EmailSender/IEmailSender.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EmailSender
+{
+    public interface IEmailSender
+    {
+        void SendEmail(Message message);
+    }
+}
